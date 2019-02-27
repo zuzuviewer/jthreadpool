7fa26cfa50cd5e299881c976e60c3bffd96b9c64
zuzuviewer <750938164@qq.com>
2019-02-27 16:52:13 +0800

    Add files via upload

---
+++ src/jthreadpool.cpp
@@ -0,0 +1,92 @@
+#include "jthreadpool.h"
+
+#include <iostream>
+#include <exception>
+#ifdef WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+JThreadPool::JThreadPool():isExit_(false)
+{
+
+}
+
+JThreadPool::JThreadPool(const unsigned int maxThreadCount, const int32_t currentThreadCount):
+    maxThreadCount_(maxThreadCount),
+    currentThreadCount_(currentThreadCount),
+    isExit_(false)
+{
+
+}
+
+JThreadPool::~JThreadPool()
+{
+    if(!threadGroup_.empty()){
+        for(auto it = threadGroup_.begin();it != threadGroup_.end();++it){
+            if(it->joinable()){
+                it->join();
+            }
+        }
+    }
+}
+
+void JThreadPool::sleep(const int32_t millSecond)
+{
+#ifdef WIN32
+    Sleep(millSecond);
+#else
+    usleep(millSecond * 1000);
+#endif
+}
+
+bool JThreadPool::addTask(const std::function<void()>&& task)
+{
+    taskQueueMutex_.lock();
+    taskQueue_.push(task);
+    taskQueueMutex_.unlock();
+    return true;
+}
+
+bool JThreadPool::start()
+{
+    if(currentThreadCount_ <= 0){
+        return false;
+    }
+    for(int32_t i = 0;i < currentThreadCount_;++i){
+        threadGroup_.push_back(std::thread(&JThreadPool::threadRun,this));
+    }
+    return true;
+}
+
+void JThreadPool::stop()
+{
+    isExit_ = true;
+}
+
+void JThreadPool::threadRun()
+{
+    for(;;){
+        if(isExit_){
+            break;
+        }
+        taskQueueMutex_.lock();
+        if(taskQueue_.empty()){
+            taskQueueMutex_.unlock();
+            //QThread::usleep(100);
+            JThreadPool::sleep(100);
+            continue;
+        }
+        const auto task = std::move(taskQueue_.back());
+        taskQueue_.pop();
+        taskQueueMutex_.unlock();
+        try {
+            task();
+        } catch (std::exception &ex) {
+            std::cout << "ThreadPool执行任务捕获到异常:" << ex.what();
+        }
+        //QThread::usleep(100);
+         JThreadPool::sleep(100);
+    }
+}
---
+++ src/jthreadpool.h
@@ -0,0 +1,43 @@
+#ifndef JTHREADPOOL_H
+#define JTHREADPOOL_H
+
+#include <thread>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <atomic>
+#include <list>
+
+/**
+ * @brief The JThreadPool class 线程池
+ */
+class JThreadPool
+{
+public:
+    JThreadPool();
+    JThreadPool(const unsigned int maxThreadCount,const int32_t currentThreadCount);
+    ~JThreadPool();
+
+public:
+    static void sleep(const int32_t millSecond);
+
+public:
+    bool addTask(const std::function<void()>&& task);
+
+    bool start();
+
+    void stop();
+
+protected:
+    void threadRun();
+
+private:
+    unsigned int maxThreadCount_ = std::thread::hardware_concurrency();
+    int32_t currentThreadCount_ = 0;//当前线程数量
+    std::queue<std::function<void()>> taskQueue_;//执行任务任务队列
+    std::mutex taskQueueMutex_;//任务队列锁
+    std::atomic_bool isExit_;//线程是否退出
+    std::list<std::thread> threadGroup_;//线程管理
+};
+
+#endif // JTHREADPOOL_H
